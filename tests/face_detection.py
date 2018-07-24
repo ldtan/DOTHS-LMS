Fix errors in image processing and tools for sign language translation

tests/face_detection.py
@@ -1,38 +1,18 @@
 # [START of Imports]
-import config
 import cv2
-import numpy as np
+from tools.capture import capture
 from tools.image_processing.face import detect_face, detect_faces
 # [END of Imports]
 
-camera = cv2.VideoCapture(config.DEFAULT_DEVICE_INDEX)
-cv2.namedWindow('Image Capture')
-
-while True:
-    _, frame = camera.read()
-
-    if not _:
-        break
-
-    # _, bounds = detect_face(frame)
-
-    # if isinstance(bounds, np.ndarray):
-    #     (x, y, w, h) = bounds
-    #     cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
-
+def detect_faces_in_frame(frame, seen_frame):
     detected = detect_faces(frame)
 
-    if detected != None:
-        for _, bounds in detected:
-            (x, y, w, h) = bounds
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
+    if detected == None:
+        return None
 
-    cv2.imshow('Image Capture', frame)
-    key = cv2.waitKey(1)
+    for _, bounds in detect_faces(frame):
+        (x, y, w, h) = bounds
+        cv2.rectangle(seen_frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
 
-    # Pressed 'esc' button.
-    if key == 27:
-        break
 
-camera.release()
-cv2.destroyAllWindows()
+capture(listeners=[detect_faces_in_frame])